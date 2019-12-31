73729f639a05e09a581fb68a06a471f64d767ffe
gradikay <gradi_kayamba@yahoo.com>
2019-12-31 07:52:29 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-dev-serverlessdeploymentbucket-1ixncboqa8sr5"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://7f8oxkjp98.execute-api.us-east-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_2uKLZ3quX",
+    APP_CLIENT_ID: "5v111qlijl49iqqm9e5j6pgmeq",
+    IDENTITY_POOL_ID: "us-east-2:c74bd017-3e14-4ac3-8365-14a03bd960df"
+  }
+};
